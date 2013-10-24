b99474f49e448c12d21962d76235ffc0aa281175
Dave Smith <da__smith@hotmail.com>
2013-10-24 15:40:10 +0100

    Added per row support which means equal heights are applied per row by default

--- active-equal-heights.src.js
+++ active-equal-heights.src.js
@@ -1,5 +1,4 @@
 
-
 /*-------------------------------------------------------------------- 
  * JQuery Plugin: "EqualHeights" & "EqualWidths"
  * by:	Scott Jehl, Todd Parker, Maggie Costello Wachs (http://www.filamentgroup.com)
@@ -17,45 +16,83 @@
  * Changelog:
  *  08.02.2007 initial Version 1.0
  *  07.24.2008 v 2.0 - added support for widths
+ *  24.10.2013 Dave Smith added perRow support
 --------------------------------------------------------------------*/
 
 $.fn.equalHeights = function(o) {
 	
 	o = o || {};
 	
-	var px = o.px || false,
-		childrenSelector = o.childrenSelector || '> *';
+	var px = o.px || false
+		,childrenSelector = o.childrenSelector || '> *'
+		,perRow = (o.perRow === undefined) ? true : o.perRow
+		;
 		
 	$(this).each(function(){
 		
-		var currentTallest = 0,
-			$children = $(this).find(childrenSelector);
+		var currentTallest
+			,$children = $(this).find(childrenSelector)
+			,currentPositionTop = -1
+			,rows = []
+			,rowsLength
+			;
+		
+		if (perRow) {
 			
-		// To be able to call this function on window resize need to reset the heights first
-		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) {
-			$children.css({'height': 'auto'});
+			$children.each(function() {
+				var $this = $(this);
+				if ($this.position().top > currentPositionTop) {
+					currentPositionTop = $this.position().top;
+					rows[rows.length] = $();
+				}
+				rows[rows.length - 1] = rows[rows.length - 1].add($this);
+			});
+					
 		}
-		$children.css({'min-height': 0}); 
+		else {
+			rows = [$children];
+		}
 		
-		$children.each(function(){
-			var $child = $(this);
-			if ($child.height() > currentTallest) {
-				currentTallest = $child.height();
+		rowsLength = rows.length;
+		
+		for (i = 0; i < rowsLength; i++) {
+			
+			currentTallest = 0;
+			
+			$children = rows[i];
+			
+			// To be able to call this function on window resize need to reset the heights first
+			// for ie6, set height since min-height isn't supported
+			if ($.browser.msie && $.browser.version == 6.0) {
+				$children.css({'height': 'auto'});
 			}
-		});
-		
-		// Fix issue in Firefox (affected v19) where it didn't always reflow floats back over to the left
-		currentTallest += 1;
-		
-		if (!px && Number.prototype.pxToEm) {
-			currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
+			
+			$children.css({'min-height': 0}); 
+			
+			$children.each(function(){
+				var $child = $(this);
+				if ($child.height() > currentTallest) {
+					currentTallest = $child.height();
+				}
+			});
+			
+			// Fix issue in Firefox (affected v19) where it didn't always reflow floats back over to the left
+			currentTallest += 1;
+			
+			if (!px && Number.prototype.pxToEm) {
+				currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
+			}
+			
+			// for ie6, set height since min-height isn't supported
+			if ($.browser.msie && $.browser.version == 6.0) {
+				$children.css({'height': currentTallest});
+			}
+			
+			$children.css({'min-height': currentTallest}); 
+			
 		}
-		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) {
-			$children.css({'height': currentTallest});
-		}
-		$children.css({'min-height': currentTallest}); 
+		
+		
 	});
 	return this;
 };
@@ -66,8 +103,9 @@ $.fn.equalWidths = function(o) {
 	
 	o = o || {};
 
-	var px = o.px || false,
-		childrenSelector = o.childrenSelector || '> *';
+	var px = o.px || false
+		,childrenSelector = o.childrenSelector || '> *'
+		;
 		
 	$(this).each(function() {
 		
@@ -94,6 +132,7 @@ $.fn.equalWidths = function(o) {
 			$children.css({'width': currentWidest});
 		}
 		$children.css({'min-width': currentWidest}); 
+		
 	});
 	return this;
 };
@@ -102,14 +141,16 @@ $.fn.equalHeightsActive = function(o) {
 	
 	o = o || {};
 	
-	var doStuff,
-		delay = o.delay || 250,
-		childrenSelector = o.childrenSelector || '> *',
-		timeoutID,
-		$this = $(this);
+	var doStuff
+		,delay = o.delay || 250
+		,childrenSelector = o.childrenSelector || '> *'
+		,timeoutID
+		,perRow = (o.perRow === undefined) ? true : o.perRow
+		,$this = $(this)
+		;
 
 	doStuff = function() {
-		$this.equalHeights({childrenSelector: childrenSelector});
+		$this.equalHeights({childrenSelector: childrenSelector, perRow: perRow});
 	};
 	
 	doStuff();
